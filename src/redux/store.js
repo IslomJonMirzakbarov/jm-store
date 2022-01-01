ce7ce45f887d56d5eb0fb727d150e215af3164f1
John Stellar <79857661+IslomJonMirzakbarov@users.noreply.github.com>
2022-01-01 23:53:23 +0900

    l removed

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,10 @@ import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
 
+if(process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
 export const store = createStore(RootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
